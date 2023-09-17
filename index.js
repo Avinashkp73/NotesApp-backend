5b0bb6e26be2380cbc9872a9048cb0cecf0fb13c
Avinash Kumar Panda <78773421+Avinashkp73@users.noreply.github.com>
2023-09-17 20:56:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const connectDB = require('./config/db');
 const notesRoutes = require('./routes/notes');
 
 const app = express();
+const cors= require('cors');
 const PORT = 5000;
 
 connectDB();
 
 //middleware
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(cors());
+app.get("/",(req,res)=>{
+    res.setHeader("Access-Control-Allow-Credentials","true");
+    res.send("API running");
+});
+//app.use(express.static('public'));
 
 app.use('/notes', notesRoutes);
 
