e6101da94050a604a6be97532b00f71200b02520
Avinash Kumar Panda <78773421+Avinashkp73@users.noreply.github.com>
2023-09-17 20:42:33 +0530

    Backend Files Added

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const notesRoutes = require('./routes/notes');
+
+const app = express();
+const PORT = 5000;
+
+connectDB();
+
+//middleware
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use('/notes', notesRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running fine on port : ${PORT} `);
+})
